5778bdff6b0ca6f4d98e9d0685f2da82551870e8
Pawan kumar <pawankr16123114@gmail.com>
2025-09-11 03:37:58 +0530

    Added some features and bg-video

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import { BrowserRouter } from "react-router-dom";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import heroBgVideo from "./assets/herobg.mp4";
 import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-          <Navbar />
-          <Hero />
+        <div className='relative overflow-hidden min-h-screen'>
+          <video
+            className="absolute inset-0 w-full h-full object-cover"
+            src={heroBgVideo}
+            autoPlay
+            loop
+            muted
+            playsInline
+            preload="auto"
+          />
+          <div className="absolute inset-0 bg-black/50" />
+          <div className="relative z-[1]">
+            <Navbar />
+            <Hero />
+          </div>
         </div>
         <About />
         <Experience />
@@ -20,7 +33,7 @@ const App = () => {
           <Contact />
 
           <StarsCanvas />
-          <Footer/>
+          <Footer />
         </div>
       </div>
     </BrowserRouter>
